f5f47500abee135af5b088e8a77dfeadf6ce52c7
Will K <willmlb@sbcglobal.net>
2017-01-20 17:18:30 -0500

    wew

--- feed2discord.py
+++ feed2discord.py
@@ -128,16 +128,17 @@ nxx = []
 if MAIN.get("random_text", "0") is 1:
     for i in ['adjectives', 'nouns']:
         with open('2syllable'+i+'.txt', 'r') as f:
-            pxx.extend(x[:-1] for x in f if x.startswith('p') and 
+            pxx.extend(x.lower()[:-1] for x in f if x.lower().startswith('p') and 
                 not (i is 'nouns' and x[-2] is 's'))
     with open('3syllablenouns.txt', 'r') as f:
-        nxx.extend(x[:-1] for x in f if x.startswith('n'))
+        nxx.extend(x.lower()[:-1] for x in f if x.lower().startswith('n'))
 else:
     for i in ['adjectives', 'nouns']:
         with open(i+'.txt', 'r') as f:
-            pxx.extend(x[:-1] for x in f if x.startswith('p'))
-            if i is 'nouns':
-                nxx.extend(x[:-1] for x in f if x.startswith('n'))
+            pxx.extend(x.lower()[:-1] for x in f if x.lower().startswith('p') and 
+                not (i is 'nouns' and x[-2] is 's'))
+    with open('nouns.txt', 'r') as f:
+        nxx.extend(x.lower()[:-1] for x in f if x.lower().startswith('n'))
 
 
 # global discord client object
